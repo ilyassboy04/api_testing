ba78ff1597271f014ae66f0843562218acaf9cad
ilyassboy04 <elsah1344@oslomet.no>
2024-08-13 19:12:12 +0200

    fungerende fetch api data (json) (img sprite)

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,2 +1,23 @@
-fetch("")
-//test
+
+async function fetchData(){
+    try{
+
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        if (!response.ok){
+            throw new Error("Failed to fetch data.");
+        }
+
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonsprite")
+
+        imgElement.src = pokemonSprite
+        imgElement.style.display = "block";
+    }
+    catch (error){
+        console.error(error);
+    }
+}
